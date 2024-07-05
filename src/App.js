8803d2ff14e970c3f74d1c09ae63ba3b410c137b
github_username <nedimmuminovic61@gmail.com>
2024-07-05 16:31:47 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+
+function Avatar(prop) {
+  return <img src={prop.link} alt="my profile" className="avatar"></img>;
+}
+
+function Intro(prop) {
+  return <p>{prop.intro}</p>;
+}
+
+function SkillList(prop) {
+  return (
+    <ul className="skill-list">
+      <li className="skill" style={{ backgroundColor: `${prop.color}` }}>
+        {prop.description}
+      </li>
+    </ul>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar link="my.jpg" />
+      <div className="data">
+        <Intro intro="dsdsfdasffffffffdassadsf"></Intro>
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList color="blue" description="Ex game developer🎮❌" />
+      </div>
     </div>
   );
 }
